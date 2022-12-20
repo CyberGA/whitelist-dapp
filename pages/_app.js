3599b14f7e8788c5decdcc3643832fd9b0b140f0
CyberGA <etomu.joshua@gmail.com>
2022-12-20 19:51:31 +0100

    refractor code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+       <Head>
+        <title>{Component.title}</title>
+        <meta name="description" content={Component.desc} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
